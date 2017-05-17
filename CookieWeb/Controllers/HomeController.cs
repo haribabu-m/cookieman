ef30a90690cdfc64f43af470a8a43ed95635de53
haribabu-mohan <hrbaharibabu@gmail.com>
2017-05-17 09:43:17 +0530

    Updated images and retrieved from cdn, included deployment profiles

--- CookieWeb/Controllers/HomeController.cs
+++ CookieWeb/Controllers/HomeController.cs
@@ -20,13 +20,6 @@ namespace CookieWeb.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
