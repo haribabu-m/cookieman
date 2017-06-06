e000e79c220fb79239af1657abf64af334323383
haribabu-mohan <hrbaharibabu@gmail.com>
2017-06-06 09:30:11 +0530

    Added a appsettings to differentiate the environment

--- CookieWeb/Controllers/HomeController.cs
+++ CookieWeb/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using InternationalCookies.Models;
 
 namespace CookieWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppSettings _appsettings;
+        public HomeController(IOptions<AppSettings> appsettings)
+        {
+            _appsettings = appsettings.Value;
+        }
         public IActionResult Index()
         {
+            string region = _appsettings.Region;
+
+            ViewData.Add("Region", region);
+            ViewBag.RegionMine = region;
             return View();
         }
 
--- CookieWeb/Startup.cs
+++ CookieWeb/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using InternationalCookies.Models;
 
 namespace CookieWeb
 {
@@ -27,6 +28,7 @@ namespace CookieWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
             // Add framework services.
             services.AddMvc();
         }
